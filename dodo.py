9160a7e3e4c68b7edf605a9feacdc8ede735982c
Wan Shen Lim <wanshen.lim@gmail.com>
2022-02-19 22:11:01 -0500

    Test.

--- dodo.py
+++ dodo.py
@@ -9,3 +9,41 @@ from dodos.forecast import *
 from dodos.noisepage import *
 from dodos.pilot import *
 from dodos.project1 import *
+
+from doit.action import CmdAction
+from plumbum import local
+
+def task_project1():
+    """
+    Generate actions.
+    """
+    def example_function(workload_csv, timeout):
+        print(f"dodo received workload CSV: {workload_csv}")
+        print(f"dodo received timeout: {timeout}")
+
+    return {
+        "actions": [
+            'python3 ./forecast/preprocessor.py --query-log-folder %(workload_csv)s --output-parquet out.parquet.gzip --output-timestamp out.timestamp.txt',
+            'PYTHONPATH=.:$PYTHONPATH python3 util/sql.py',
+            example_function,
+            'echo "SELECT 1;" > actions.sql',
+            'echo "SELECT 2;" >> actions.sql',
+            'echo \'{"VACUUM": true}\' > config.json',
+        ],
+        "uptodate": [False],
+        "verbosity": 2,
+        "params": [
+            {
+                "name": "workload_csv",
+                "long": "workload_csv",
+                "help": "The PostgreSQL workload to optimize for.",
+                "default": None,
+            },
+            {
+                "name": "timeout",
+                "long": "timeout",
+                "help": "The time allowed for execution before this dodo task will be killed.",
+                "default": None,
+            },
+        ],
+    }
---
+++ util/sql.py
@@ -0,0 +1,146 @@
+
+from dataclasses import dataclass
+import json
+
+from pprint import pprint
+import pglast.parser
+
+import psycopg
+
+from forecast.preprocessor import Preprocessor
+
+
+@dataclass
+class Column:
+    name: str
+    dtype: str
+    active: bool = False
+
+def get_catalog():
+    query = "SELECT table_name, column_name, udt_name FROM information_schema.columns where table_schema='public';"
+    catalog = {}
+    with psycopg.connect("dbname=project1db user=project1user password=project1pass") as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            res = cur.fetchall()
+            for table, col, dtype in res:
+                catalog[table] = catalog.get(table, [])
+                catalog[table].append(Column(col, dtype))
+    return catalog
+
+
+def item_generator(json_input, lookup_key):
+    if isinstance(json_input, dict):
+        for k, v in json_input.items():
+            if k == lookup_key:
+                yield v
+            else:
+                yield from item_generator(v, lookup_key)
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from item_generator(item, lookup_key)
+
+def grab(j, key):
+    if isinstance(j, list):
+        for item in j:
+            yield from grab(item, key)
+    elif isinstance(j, dict):
+        for k, v in j.items():
+            if k == key:
+                yield v
+            else:
+                yield from grab(v, key)
+
+def _parse_select_stmt(catalog, query, sel_json):
+    # Get tables (table name -> list of aliases).
+    rangevars = list(grab(sel_json, 'RangeVar'))
+    tables = set()
+    aliases = {}
+    for rv in rangevars:
+        table = rv['relname']
+        tables.add(table)
+        if 'alias' in rv:
+            alias = rv['alias']['aliasname']
+            aliases[alias] = aliases.get(alias, set())
+            aliases[alias].add(table)
+
+    wheres = list(grab(sel_json, 'whereClause'))
+    columnrefs = list(grab(wheres, 'ColumnRef'))
+    targets = []
+    for cr in columnrefs:
+        fields = cr['fields']
+        if len(fields) == 1:
+            if 'A_Star' in fields[0]:
+                targets.append('*')
+            else:
+                col = fields[0]['String']['str']
+                targets.append(col)
+        elif len(fields) == 2:
+            alias = fields[0]['String']['str']
+            col = fields[1]['String']['str']
+            if alias not in aliases:
+                # This can happen by naming intermediates.
+                targets.append((alias, col))
+            else:
+                # If there is only one alias, resolve it now.
+                candidate_aliases = aliases[alias]
+                if len(candidate_aliases) == 1:
+                    targets.append((list(candidate_aliases)[0], col))
+                else:
+                    targets.append((alias, col))
+        else:
+            raise RuntimeError("What is this? fields: {}, JSON: {}".format(fields, sel_json))
+
+    def mark(table, column):
+        if not column.active:
+            print(f'Marking {table}.{column.name} active because of {query}.')
+        column.active = True
+
+    for target in targets:
+        if isinstance(target, str):
+            if target == '*':
+                for table in tables:
+                    for column in catalog.get(table, []):
+                        mark(table, column)
+            else:
+                for table in [c for c in catalog if c in tables]:
+                    for column in catalog[table]:
+                        if column.name == target:
+                            mark(table, column)
+        elif isinstance(target, tuple):
+            target_table, target_col = target
+            for column in catalog.get(target_table, []):
+                if column.name == target_col:
+                    mark(target_table, column)
+
+
+def _parse_update_stmt(catalog, query, upd_json):
+    pass
+
+def process(catalog, query):
+    # Parse the query with pglast.
+    query_json = json.loads(pglast.parser.parse_sql_json(query))
+    for statement_json in query_json['stmts']:
+        statement = statement_json['stmt']
+        statement_keys = list(statement.keys())
+        assert len(statement_keys) == 1
+        statement_type = statement_keys[0]
+        # print(statement_type, query)
+        if statement_type == 'SelectStmt':
+            _parse_select_stmt(catalog, query, statement[statement_type])
+        elif statement_type == 'UpdateStmt':
+            _parse_update_stmt(catalog, query, statement[statement_type])
+        else:
+            raise RuntimeError("Unknown statement type {}, query: {}.".format(statement_type, query))
+
+def main():
+    catalog = get_catalog()
+    pp = Preprocessor(parquet_path='out.parquet.gzip')
+    sqls = set(pp.get_grouped_dataframe_params().index.get_level_values(0))
+    for sql in sqls:
+        process(catalog, sql)
+        print(sql)
+    pprint(catalog)
+
+if __name__ == '__main__':
+    main()
